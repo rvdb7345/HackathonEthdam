686376b0620473dd95a55430c109f2a4aa2f9671
Hannes <hannes@Hanness-MacBook-Pro.local>
2023-05-20 16:02:51 +0200

    coingecko_fix

--- data_gathering/fetch_coingecko.py
+++ data_gathering/fetch_coingecko.py
@@ -1,26 +1,32 @@
 import datetime
-import requests
-
+from fetch_api import fetch_api
 
 def fetch_coingecko():
-  coins = []
-  current_date = datetime.date.today()
-  week_ago_date = current_date - datetime.timedelta(days=7)
+    coins = {}
+    current_date = datetime.date.today()
+    week_ago_date = current_date - datetime.timedelta(days=7)
 
-  current_url = f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en' 
+    current_url = f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en' 
 
-  current_data = read_api(current_url)
-  for i in current_data[:10]:
-    id = i['id']
-    current_price = i['current_price']
-    coins[id] = {'current_price' : current_price}
+    current_data = fetch_api(current_url, url_settings=None)
+    for i in current_data[:10]:
+        try:
+            coin_id = i['id']
+            current_price = i['current_price']
+            coins[coin_id] = {'current_price': current_price}
+        except Exception as e:
+            print(str(i) + ' ' + str(e))
 
-  for coin in coins.keys():
-    data = read_api(f'https://api.coingecko.com/api/v3/coins/{coin}/history?date={week_ago_date}')
-    coins[coin].update({'history_price': data['market_data']['current_price']['usd']})
+    for coin in coins.keys():
+        try:
+            data = fetch_api(f'https://api.coingecko.com/api/v3/coins/{coin}/history?date={week_ago_date}', url_settings=None)
+            if data is not None and 'market_data' in data and 'current_price' in data['market_data'] and 'usd' in data['market_data']['current_price']:
+                coins[coin].update({'history_price': data['market_data']['current_price']['usd']})
+        except Exception as e:
+            print(f'Error fetching data for {coin}: {str(e)}')
 
-  return coins
+    return coins
 
 if __name__ == "__main__":
-  results = fetch_coingecko()
-  print(results)
+    results = fetch_coingecko()
+    print(results)
