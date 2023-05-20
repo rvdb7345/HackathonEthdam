c3b15a081a6efad704c5fafd4bc3696f773efcfc
beau.furnee <beau.furnee@pwc.com>
2023-05-20 18:10:59 +0200

    Working gecko code

--- data_gathering/fetch_api.py
+++ data_gathering/fetch_api.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import time
 
 
-def fetch_api(url, url_settings):
+def fetch_api(url, url_settings=None, recursive_check=False):
     # The URL of the API endpoint
     # Send a GET request to the API endpoint
     response = requests.get(url)
@@ -11,10 +12,16 @@ def fetch_api(url, url_settings):
     if response.status_code == 200:
         # Get the data from the response
         data = response.json()
-
-        # Print the data
-        print(data)
+    elif response.status_code == 429:
+        if not recursive_check:
+            # Wait for the API to cool down
+            print("Waiting because the gecko is sleeping....")
+            time.sleep(70)
+            data = fetch_api(url, recursive_check=True)
+        else:
+            print("Gecko keeps sleeping, ABORTING.")
+            return False
     else:
-        print(f'Request failed with status code {response.status_code}')
+        print(f"Request failed with status code {response.status_code}")
 
     return data
