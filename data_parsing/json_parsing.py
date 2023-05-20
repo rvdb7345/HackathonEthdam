e8d9d20adbead9ea3ee116a9a3b7ad264982521b
LouisWW <louis.weyland@hotmail.com>
2023-05-20 21:26:34 +0200

    Added markdown format

--- data_parsing/json_parsing.py
+++ data_parsing/json_parsing.py
@@ -6,15 +6,25 @@ def convert_json_to_rds(json_data):
 
     return rds_news
 
-def convert_json_to_markdown(json_data,convert_dates=False):
+def convert_to_markdown(json_data,convert_dates=False):
     """Convert data to markdown format for beau."""
     if isinstance(json_data,pd.DataFrame):
         rds_news = json_data
     elif isinstance(json_data,str):
         rds_news = convert_json_to_rds(json_data)
-        
-    rds_news.columns = rds_news.columns.str.replace('link', 'hyperlink')
-    samples= rds_news[['title','hyperlink','keywords','content']].sample(n=10)
-    return samples.to_dict()
+            
+    rds_news.columns = rds_news.columns.str.replace('publisher_domain', 'hyperlink')
+    samples= rds_news[['title','hyperlink','keywords','content']].sample(n=2)
+    
+    data=[]
+    for index, row in samples.iterrows():
+        json_object = {
+        "title": row['title'],
+        "hyperlink": row['hyperlink'],
+        "keywords": row['keywords'],
+        "content": row['content']
+    }
+    data.append(json_object)
+    return json_data
 
 
