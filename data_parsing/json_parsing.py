7d437bda04274476c438e7149602d9a9d90af761
LouisWW <louis.weyland@hotmail.com>
2023-05-20 21:06:18 +0200

    Added random parsin

--- data_parsing/json_parsing.py
+++ data_parsing/json_parsing.py
@@ -6,4 +6,14 @@ def convert_json_to_rds(json_data):
 
     return rds_news
 
+def convert_json_to_markdown(json_data,convert_dates=False):
+    """Convert data to markdown format for beau."""
+    if isinstance(json_data,pd.DataFrame):
+        rds_news = json_data
+    elif isinstance(json_data,str):
+        rds_news = convert_json_to_rds(json_data)
+        
+    rds_news.columns = rds_news.columns.str.replace('link', 'hyperlink')
+    return rds_news[['title','hyperlink','keywords','content']].sample(n=10)
+
 
