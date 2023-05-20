0d2473d4619db58c05ee59fb76d604b3b38fc5cd
LouisWW <louis.weyland@hotmail.com>
2023-05-20 14:41:51 +0200

    Added goglenews

---
+++ data_gathering/fetch_googlenews.py
@@ -0,0 +1,11 @@
+
+from pygooglenews import GoogleNews
+
+
+
+def fetch_googlenews():
+    """Fetch data from google news."""
+    gn = GoogleNews()
+    business = gn.search('crypto')
+
+    return business["entries"]
