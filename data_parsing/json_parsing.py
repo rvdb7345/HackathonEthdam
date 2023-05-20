28f8143d1141773964742d489b5283529c341ce7
rvdb7345 <rvdb7345@gmail.com>
2023-05-20 15:59:08 +0200

    data update

--- data_parsing/json_parsing.py
+++ data_parsing/json_parsing.py
@@ -1,18 +1,8 @@
 import json
 import pandas as pd
 
-def load_all_news(file_path):
-
-    # Opening JSON file
-    with open(file_path) as f:
-        # returns JSON object as
-        # a dictionary
-        data = json.load(f)
-
-    return data
-
 def convert_json_to_rds(json_data):
-    rds_news = pd.read_json(json_data)
+    rds_news = pd.read_json(json_data, convert_dates=False)
 
     return rds_news
 
--- infer_keywords.py
+++ infer_keywords.py
@@ -1,12 +1,17 @@
 import pandas as pd
+from tqdm import tqdm
 from data_augmentation.keyword_extraction import KeywordGenerator
 
 if __name__ == '__main__':
     keyword_generator = KeywordGenerator()
-
     formatted_news = pd.read_csv('data/20230520_combined.csv', engine='python')
-    formatted_news.dropna(subset=['content'], inplace=True)
 
-    print(formatted_news.iloc[0]['content'])
-    keywords = keyword_generator.create_keywords_for_doc(formatted_news.iloc[0]['content'])
-    print(keywords)
+    for idx, row in tqdm(formatted_news.iterrows(), total=len(formatted_news)):
+        if not pd.isna(row['content']):
+            keywords = keyword_generator.create_keywords_for_doc(row['content'])
+            formatted_news.loc[idx, 'algorithmic_keywords'] = str([keyword[0] for keyword in keywords])
+
+    formatted_news.to_csv('data/20230520_combined_keywords.csv')
+
+
+
