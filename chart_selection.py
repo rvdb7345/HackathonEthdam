565cfd02fa2cc69ff589d77b82576bd9eb771c12
beau.furnee <beau.furnee@pwc.com>
2023-05-21 12:23:49 +0200

    Chart selection

--- chart_selection.py
+++ chart_selection.py
@@ -1,4 +1,5 @@
 #  File for selecting charts from dune
+import pandas as pd
 
 
 def get_topics_from_articles(articles):
@@ -6,11 +7,31 @@ def get_topics_from_articles(articles):
     pass
 
 
-def get_charts(relevant_topics):
+def get_charts(relevant_topics, chart_topics="charts_topics.csv"):
     # Selects charts based on given topics
-    pass
+    charts_topics_df = pd.read_csv(chart_topics)
+
+    set1 = set(relevant_topics)
+    # Iterate over the rows of the dataframe
+    for index, row in charts_topics_df.iterrows():
+        set2 = set(row["topics"])
+        common_elements = set1.intersection(set2)
+        if len(common_elements) == 0:
+            # Drop the row if the 'City' column value is 'London'
+            charts_topics_df = charts_topics_df.drop(index)
+    return charts_topics_df
 
 
 def create_chart_file(selected_charts):
     # Outputs a text file to be merged into the final final text file
-    pass
+
+    # Open a file in write mode
+    with open("final_charts.txt", "a") as file:
+        # Write content to the file
+        file.write("## Relevant graphs of the week\n")
+        for i, chart in enumerate(selected_charts):
+            file.write("Graph {}: {}".format(i, chart["name"]))
+            file.wirte(str(chart["link"]))
+
+
+get_charts([1, 2, 3])
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ create the newsletter.
 
 # Imports
 from create_news_selection import select_representative_documents
-from data_parsing.json_parsing import convert_json_to_markdown,convert_json_to_rds
+from data_parsing.json_parsing import convert_json_to_markdown, convert_json_to_rds
 from json_to_markdown import article_json_to_markdown
 from generate_news_overview import generate_news_overview
 from data_augmentation.classifying_data import grouping_news_article_per_week
 from create_visual.create_cointable_image import create_cointable_image
-from  datetime import datetime
+from datetime import datetime
 import pandas as pd
+
 
 class CryptoChronicles:
     def __init__(self):
@@ -28,25 +29,23 @@ class CryptoChronicles:
 
         return df
 
-
     def select_articles(self, news_last_week):
-        forbidden_subjects = [
-            'best_crypto',
-            'best_2023'
-        ]
+        forbidden_subjects = ["best_crypto", "best_2023"]
 
-        articles_to_publish = select_representative_documents(news_last_week,
-                                                              forbidden_subjects=forbidden_subjects,
-                                                              num_articles_to_publish=10,
-                                                              plot_clusters=False)
-
+        articles_to_publish = select_representative_documents(
+            news_last_week,
+            forbidden_subjects=forbidden_subjects,
+            num_articles_to_publish=10,
+            plot_clusters=False,
+        )
+        print(articles_to_publish)
         return articles_to_publish
 
     def gather_high_metrics(self):
         # Gather the high level metrics
         create_cointable_image()
 
-    def format_data(self,df:pd.DataFrame)->None:
+    def format_data(self, df: pd.DataFrame) -> None:
         # Turn the gathered data to a nice format
         markdown_json = convert_json_to_markdown(df)
         article_json_to_markdown(markdown_json)
