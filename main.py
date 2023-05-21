4e98fbe44169d3443b4c01ab021de932db620d3d
beau.furnee <beau.furnee@pwc.com>
2023-05-21 09:51:19 +0200

    main now a class

--- main.py
+++ main.py
@@ -8,20 +8,31 @@ from data_parsing.json_parsing import convert_json_to_markdown
 from json_to_markdown import article_json_to_markdown
 
 
-def main():
-    # Gather the high level metrics
-    pass
+class CryptoChronicles:
+    def init(self):
+        pass
 
-    # Gather the newsdata
-    markdown_json = convert_json_to_markdown("data/20230520_combined.json")
-    article_json_to_markdown(markdown_json)
+    def gather_article_data(self):
+        # Function to create a new dataset by calling API's
+        pass
 
-    # Gather the dune chart
-    pass
+    def gather_high_metrics(self):
+        # Gather the high level metrics
+        pass
 
-    # Create the output text file to be copied into Mirror
-    pass
+    def format_data(self):
+        # Turn the gathered data to a nice format
+        markdown_json = convert_json_to_markdown("data/20230520_combined.json")
+        article_json_to_markdown(markdown_json)
+
+    def gather_dune_charts(self):
+        # Gather the dune charts
+        pass
+
+    def create_final_output(self):
+        # Create the output text file to be copied into Mirror
+        pass
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
